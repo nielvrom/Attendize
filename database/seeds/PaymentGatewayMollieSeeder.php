60bf236415ad0477d61522e2040213eb5e83c782
Niels Vroman <vroman.niels@hotmail.com>
2017-05-23 07:01:09 +0000

    add Mollie gateway provider

---
+++ database/seeds/PaymentGatewayMollieSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PaymentGatewayMollieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $payment_gateways = [
+            [
+                'id' => 5,
+                'name' => 'Mollie',
+                'provider_name' => 'Mollie',
+                'provider_url' => 'https://www.mollie.com',
+                'is_on_site' => 1,
+                'can_refund' => 1,
+            ],
+        ];
+
+        DB::table('payment_gateways')->insert($payment_gateways);
+    }
+}
